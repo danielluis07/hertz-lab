6baeeff738db8c74b71a5244147cf4f833ce65d6
danielluis07 <111083286+danielluis07@users.noreply.github.com>
2026-09-01 14:52:50 -0300

    base setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { cn } from "@/lib/utils";
+import { geistSans, inter } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,9 +11,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      lang="pt-BR"
+      className={cn(
+        "h-full antialiased font-sans",
+        geistSans.variable,
+        inter.variable,
+      )}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
